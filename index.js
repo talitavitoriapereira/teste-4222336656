7ac72bf1bf3d05834a6d34d654b3512f30670e0e
talitavitoriapereira <159267033+talitavitoriapereira@users.noreply.github.com>
2025-08-13 11:14:43 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const db = new sqlite3.Database('./database.db', (err) => {
 db.serialize(() => {
     db.run(`
         CREATE table if not EXISTS aluno(
-        id_aluno integer AUTOINCREMENT,
+        id_aluno, 
   cgm integer PRIMARY KEY  NOT null UNIQUE,
   nome varchar (400) not NULL,
   nascimento date not NULL, 
